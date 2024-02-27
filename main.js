b08dcfb3909c231aa2989908152135d9e4c32bff
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-27 21:39:18 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-var guestList = ["hamna", "fiza", "amna"];
-guestList.forEach(function (OneGuest) { return console.log("Salam ".concat(OneGuest, ", would you like to dinner with me?")); });
+var guestList = ["hamna", "fiz", "minhaj", "aijaz"];
+var dontCome = guestList[0];
+console.log(dontCome, "nh ahh skti");
+guestList.splice(0, 1, "yousof");
+guestList.forEach(function (guest) { return console.log("salam ".concat(guest, ", would you like to dinner with me/")); });
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-let guestList = ["hamna" , "fiza" , "amna"];
-guestList.forEach(OneGuest => console.log(`Salam ${OneGuest}, would you like to dinner with me?`));
-
-export{guestList}
+let guestList = ["hamna", "fiz", "minhaj", "aijaz"];
+let dontCome = guestList[0];
+console.log(dontCome, "nh ahh skti");
+guestList.splice(0, 1, "yousof");
+guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
