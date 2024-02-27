813faac122ca51bf05d000cc0ec6f07eef7dd6ea
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-27 21:52:38 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var guestList = ["hamna", "fiz", "minhaj", "aijaz"];
 var dontCome = guestList[0];
-console.log(dontCome, "nh ahh skti");
+// console.log(dontCome, "nh ahh skti");
 guestList.splice(0, 1, "yousof");
 guestList.forEach(function (guest) { return console.log("salam ".concat(guest, ", would you like to dinner with me/")); });
 guestList.unshift("amna", "iqra", "warda");
-guestList.splice(0, 1, "yousof");
-guestList.forEach(function (guest) { return console.log("salam ".concat(guest, ", would you like to dinner with me/")); });
+// guestList.splice(0, 1, "yousof");
+// guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
+console.log("\nUnortunately we can not arrange big table , Onlytwo people allow.");
+while (guestList.length > 2) { }
+var remove_guest = guestList.pop();
+console.log("sorryy sir/madam.".concat(remove_guest, " you are not invited for dinner."));
+// guestList.splice(0, 1, "yousof");
+// guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
+guestList.splice(0, 2);
+console.log(guestList);
--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 let guestList = ["hamna", "fiz", "minhaj", "aijaz"];
 let dontCome = guestList[0];
-console.log(dontCome, "nh ahh skti");
+// console.log(dontCome, "nh ahh skti");
 guestList.splice(0, 1, "yousof");
 guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
 
 guestList.unshift("amna", "iqra", "warda");
-guestList.splice(0, 1, "yousof");
-guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
+// guestList.splice(0, 1, "yousof");
+// guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
+
+console.log("\nUnortunately we can not arrange big table , Onlytwo people allow.")
+while(guestList.length>2){}
+let remove_guest = guestList.pop();
+console.log(`sorryy sir/madam.${remove_guest} you are not invited for dinner.`);
+
+// guestList.splice(0, 1, "yousof");
+// guestList.forEach(guest => console.log(`salam ${guest}, would you like to dinner with me/`));
+
+guestList.splice(0,2)
+console.log(guestList)
