ad7c5c3336c7df5eae0092217ff32b483f4c0bdf
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-27 23:13:34 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-var favourite_fruits = ["kivi", "orange", "apples", "berry", "peach"];
-if (favourite_fruits.includes("kivi")) {
-    console.log("kivi");
-}
-if (favourite_fruits.includes("peach")) {
-    console.log("you really like banana");
-}
-if (favourite_fruits.includes("orange")) {
-    console.log("orange");
-}
-if (favourite_fruits.includes("berry")) {
-    console.log("you really like banana");
-}
-if (favourite_fruits.includes("apples")) {
-    console.log("apples");
+//let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
+//for(let user of users) {
+//   if (user === "hamna") {
+//     console.log('Hello hamna, would you like to see a status report?')
+// } else {
+//     console.log('Hello ${user}, thank you for loggin in again') 
+// }
+//}
+//Q.31
+var users = ["hamna", "amna", "aijaz", "night", "arshad"];
+//if (users.length === 0) {
+// console.log ("we need to find users some users!")   
+//} else {
+//  for (let user of users){
+//      if ( user === "hamna") {
+//          console.log('Hello hamna, would you like to see a status report?') 
+//         } else {
+//         console.log('Hello ${user}, thank you for loggin in again')
+//          }
+//     }
+//  }
+//users = []
+//if (users.length === 0) {
+//  console.log ("we need to find some users!")
+//}
+//Q.32
+var current_users = ["admin", "Eric", "Ali", "Hamza", "fatima"];
+var new_users = ["admin", "fatima", "Aliza", "Hasseb", "Noor"];
+var current_user_lower = current_users.map(function (user) { return user.toLowerCase(); });
+for (var _i = 0, new_users_1 = new_users; _i < new_users_1.length; _i++) {
+    var new_user = new_users_1[_i];
+    if (current_user_lower.includes(new_user.toLowerCase())) {
+        console.log("Sorry ".concat(new_user, ", that name is taken"));
+    }
+    else {
+        console.log("yes ".concat(new_user, ", is still in avaliable list"));
+    }
 }
--- main.ts
+++ main.ts
@@ -1,20 +1,40 @@
-let favourite_fruits : string [] = ["kivi", "orange", "apples", "berry", "peach"]
-if (favourite_fruits.includes("kivi")) {
-console.log("kivi")
-}
-if (favourite_fruits.includes("peach")) {
-    console.log("you really like banana")
+//let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
+//for(let user of users) {
+ //   if (user === "hamna") {
+   //     console.log('Hello hamna, would you like to see a status report?')
+   // } else {
+   //     console.log('Hello ${user}, thank you for loggin in again') 
+   // }
+//}
+
+//Q.31
+let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
+
+//if (users.length === 0) {
+// console.log ("we need to find users some users!")   
+//} else {
+  //  for (let user of users){
+  //      if ( user === "hamna") {
+  //          console.log('Hello hamna, would you like to see a status report?') 
+    
+   //         } else {
+   //         console.log('Hello ${user}, thank you for loggin in again')
+  //          }
+   //     }
+  //  }
+//users = []
+//if (users.length === 0) {
+  //  console.log ("we need to find some users!")
+//}
+
+//Q.32
+let current_users : string [] = ["admin", "Eric", "Ali", "Hamza", "fatima"]
+let new_users : string [] = ["admin", "fatima", "Aliza", "Hasseb", "Noor"]
+let current_user_lower : string [] =current_users.map(user => user.toLowerCase())
+for (let new_user of new_users) {
+    if (current_user_lower.includes (new_user.toLowerCase())) {
+        console.log(`Sorry ${new_user}, that name is taken`)
+    } else {
+        console.log(`yes ${new_user}, is still in avaliable list`)
     }
-    if (favourite_fruits.includes("orange")) {
-        console.log("orange")
-        }
-        if (favourite_fruits.includes("berry")) {
-            console.log("you really like banana")
-            }
-            if (favourite_fruits.includes("apples")) {
-                console.log("apples")
-            }
-                
-            
-                
-                        
+}
