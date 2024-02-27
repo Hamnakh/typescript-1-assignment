b3db14fd5d83c7a78aa652b257e8cf04f88c0279
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-27 22:40:51 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,47 +1,7 @@
-let name_1 : string = "hamna"
-let name_2 : string = "hamna khan"
-let name_3 : string = "miss hamna khan"
+//let allen_colour = "green"
+//if (allen_colour == "green")
+//console.log("you earn 5 points")
 
-//if (name_1 == name_2){
- //   console.log("names are equal")
-//} else {
-//    console.log("names are not equal)
-
-
-//if (name_1 != name_2){
- //   console.log("names are equal")
-//}
-
-//if (name_1 != name_3){
-    //   console.log("names are equal")
-   //}
-   let age_1 : number = 18
-   let age_2 : number = 22
-
-   //if (age _1 == 18) {
-  //  console.log("eligible for vote")
-  // }
-
-   
-   //if (age_1 != 22) {
-   // console.log("eligible for vote in older category")
-  // }
-  //if (age_1 <= age_2){ //less
-   // console.log("younger")
- // }
-  //if (age_2 >= age_1){ //greater
-    //console.log("older")
- // }
-//if (age_1 == 18 && age_2 == 22){
-  //  console.log("person is eligible for vote")
-//}
-//if (age_1 == 18 || age_2 != 22){
-   // console.log("person is eligible not for vote")
-//}
-let country : string [] = ["pakistan", "india", "japan", "china"]
-//if (country.includes("pakistan")){
-  //  console.log("pakistan is in countryy list")
-//}
-if (!country.includes ("america")){
-    console.log("america is not includes in an arrayy")
-}
+let allen_colour : string = "red"
+if (allen_colour == "green")
+console.log("no output")
