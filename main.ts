ed64dcd257b0c082584dd1e019060e41bb7cd506
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-27 22:39:31 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,25 +1,47 @@
-let car ="subaru";
+let name_1 : string = "hamna"
+let name_2 : string = "hamna khan"
+let name_3 : string = "miss hamna khan"
 
-console.log("is car == 'subaru'? predict false")
-console.log(car == 'subaru')
+//if (name_1 == name_2){
+ //   console.log("names are equal")
+//} else {
+//    console.log("names are not equal)
 
-console.log("is car == 'SUBARU'? predict false")
-console.log(car == 'SUBARU')
 
-console.log("is car.length == 6? predict true")
-console.log(car.length == 6)
+//if (name_1 != name_2){
+ //   console.log("names are equal")
+//}
 
-console.log("is car.length !== 10? predict true")
-console.log(car.length !== 10)
+//if (name_1 != name_3){
+    //   console.log("names are equal")
+   //}
+   let age_1 : number = 18
+   let age_2 : number = 22
 
-console.log("is 10 > 45 ? predict false")
-console.log(10 > 45)
+   //if (age _1 == 18) {
+  //  console.log("eligible for vote")
+  // }
 
-console.log("is 3 <= 2 ? predict false")
-console.log(3 <= 2)
-
-console.log("is 72 >= 83 ? predict false")
-console.log(72 >= 83)
-
-console.log("is car == 'subaru' && car.length ==6? predict true")
-console.log(car == 'subaru' && car.length == 6)
+   
+   //if (age_1 != 22) {
+   // console.log("eligible for vote in older category")
+  // }
+  //if (age_1 <= age_2){ //less
+   // console.log("younger")
+ // }
+  //if (age_2 >= age_1){ //greater
+    //console.log("older")
+ // }
+//if (age_1 == 18 && age_2 == 22){
+  //  console.log("person is eligible for vote")
+//}
+//if (age_1 == 18 || age_2 != 22){
+   // console.log("person is eligible not for vote")
+//}
+let country : string [] = ["pakistan", "india", "japan", "china"]
+//if (country.includes("pakistan")){
+  //  console.log("pakistan is in countryy list")
+//}
+if (!country.includes ("america")){
+    console.log("america is not includes in an arrayy")
+}
