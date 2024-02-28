ba75aeefca25d0a06f6cfa068785aa02f4e52a3c
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-29 00:18:54 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,10 @@
-//let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
-//for(let user of users) {
-//   if (user === "hamna") {
-//     console.log('Hello hamna, would you like to see a status report?')
-// } else {
-//     console.log('Hello ${user}, thank you for loggin in again') 
-// }
-//}
-//Q.31
 var users = ["hamna", "amna", "aijaz", "night", "arshad"];
-if (users.length === 0) {
- console.log ("we need to find users some users!")   
-} else {
-  for (let user of users){
-     if ( user === "hamna") {
-          console.log('Hello hamna, would you like to see a status report?') 
-         } else {
-        console.log('Hello ${user}, thank you for loggin in again')
-          }
-     }
-  }
-users = []
-if (users.length === 0) {
-  console.log ("we need to find some users!")
+for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+    var user = users_1[_i];
+    if (user === "hamna") {
+        console.log('Hello hamna, would you like to see a status report?');
+    }
+    else {
+        console.log('Hello ${user}, thank you for loggin in again');
+    }
 }
-//Q.32
-//var current_users = ["admin", "Eric", "Ali", "Hamza", "fatima"];
-//var new_users = ["admin", "fatima", "Aliza", "Hasseb", "Noor"];
-//var current_user_lower = current_users.map(function (user) { return user.toLowerCase(); });
-//for (var _i = 0, new_users_1 = new_users; _i < new_users_1.length; _i++) {
- //   var new_user = new_users_1[_i];
- //   if (current_user_lower.includes(new_user.toLowerCase())) {
- //       console.log("Sorry ".concat(new_user, ", that name is taken"));
- //   }
- //   else {
- //       console.log("yes ".concat(new_user, ", is still in avaliable list"));
- //   }
-//}
--- main.ts
+++ main.ts
@@ -1,40 +1,8 @@
-//let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
-//for(let user of users) {
- //   if (user === "hamna") {
-   //     console.log('Hello hamna, would you like to see a status report?')
-   // } else {
-   //     console.log('Hello ${user}, thank you for loggin in again') 
-   // }
-//}
-
-//Q.31
 let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
-
-//if (users.length === 0) {
-// console.log ("we need to find users some users!")   
-//} else {
-  //  for (let user of users){
-  //      if ( user === "hamna") {
-  //          console.log('Hello hamna, would you like to see a status report?') 
-    
-   //         } else {
-   //         console.log('Hello ${user}, thank you for loggin in again')
-  //          }
-   //     }
-  //  }
-//users = []
-//if (users.length === 0) {
-  //  console.log ("we need to find some users!")
-//}
-
-//Q.32
-let current_users : string [] = ["admin", "Eric", "Ali", "Hamza", "fatima"]
-let new_users : string [] = ["admin", "fatima", "Aliza", "Hasseb", "Noor"]
-let current_user_lower : string [] =current_users.map(user => user.toLowerCase())
-for (let new_user of new_users) {
-    if (current_user_lower.includes (new_user.toLowerCase())) {
-        console.log(`Sorry ${new_user}, that name is taken`)
-    } else {
-        console.log(`yes ${new_user}, is still in avaliable list`)
-    }
+for(let user of users) {
+   if (user === "hamna") {
+    console.log('Hello hamna, would you like to see a status report?')
+ } else {
+     console.log('Hello ${user}, thank you for loggin in again') 
+ }
 }
