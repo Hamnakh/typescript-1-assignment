2fcf7c091f770cbcd28d77e7bded829504959ea4
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-28 20:57:54 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-function show_magicians(magicians: string[]): void {
-    for (const magician of magicians) {
-        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+function make_great(magicians: string[]): void {
+    for (let i = 0; i < magicians.length; i++) {
+       magicians[i] = magicians[i]  +  'the great'
     }
 }
-const magician: string[] = ["ali", "hamza", "bilal"];
-show_magicians(magician)
+const magicians2: string[] = ["usman", "hasseb", "wajaht"];
+make_great(magicians2)
+console.log(magicians2)
