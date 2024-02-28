4ab76b6a052bbf135caffa728887e16b6846d574
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-28 20:56:43 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,44 +1,8 @@
-//function makeshirt(size: string, text: string): void {
-//    console.log(`you order a ${size} shirt that says ${text}`)
-//}
-//makeshirt ('large','"i love typescript"')
-//makeshirt('medium','"i need a big shirt"')
-//Q.37
-//function makeshirt(size: string = 'large', text: string = ' I love typescript'): void {
-//console.log(`you have order a ${size}, shirt that says ${text}`)
-//}
-//makeshirt();
-//makeshirt('medium')
-//different message
-//makeshirt('small', 'I need a big shirt to wear')
-//Q.38
-//function describe_city(city: string, country: string = 'Pakistan'): void {
-//   console.log(`${city}  ${country}`)
-//}
-//describe_city('Karachi')
-//describe_city('France', 'Europe')
-//describe_city('Lahore', 'punjab')
-//Q.39
-//function cityCountry(city: string, country: string):string { 
-//    return `${city}, ${country}`
-//}
-//let c1 = cityCountry('lahore', 'pakistan')
-//let c2 = cityCountry('tokyo', 'japan')
-//let c3 = cityCountry('paris', 'france')
-//console.log(c1)
-//console.log(c2)
-//console.log(c3)
-//Q.40
-function makeAlbum(artist, title) {
-    var dictionaries = {
-        artist: artist.charAt(0).toUpperCase() + artist.slice(1),
-        title: title.charAt(0).toUpperCase() + title.slice(1)
-    };
-    return dictionaries;
+function show_magicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician_1 = magicians_1[_i];
+        console.log(magician_1.charAt(0).toUpperCase() + magician_1.slice(1));
+    }
 }
-var album = makeAlbum("ali", "light");
-console.log(album);
-album = makeAlbum("bilal", "red wave");
-console.log(album);
-album = makeAlbum("hamza", "amna");
-console.log(album);
+var magician = ["ali", "hamza", "bilal"];
+show_magicians(magician);
--- main.ts
+++ main.ts
@@ -1,49 +1,7 @@
-//function makeshirt(size: string, text: string): void {
-//    console.log(`you order a ${size} shirt that says ${text}`)
-//}
-//makeshirt ('large','"i love typescript"')
-//makeshirt('medium','"i need a big shirt"')
-
-//Q.37
-//function makeshirt(size: string = 'large', text: string = ' I love typescript'): void {
-//console.log(`you have order a ${size}, shirt that says ${text}`)
-//}
-//makeshirt();
-//makeshirt('medium')
-
-//different message
-//makeshirt('small', 'I need a big shirt to wear')
-
-//Q.38
-//function describe_city(city: string, country: string = 'Pakistan'): void {
- //   console.log(`${city}  ${country}`)
-//}
-//describe_city('Karachi')
-//describe_city('France', 'Europe')
-//describe_city('Lahore', 'punjab')
-
-//Q.39
-//function cityCountry(city: string, country: string):string { 
-//    return `${city}, ${country}`
-//}
-//let c1 = cityCountry('lahore', 'pakistan')
-//let c2 = cityCountry('tokyo', 'japan')
-//let c3 = cityCountry('paris', 'france')
-//console.log(c1)
-//console.log(c2)
-//console.log(c3)
-
-//Q.40
-function makeAlbum (artist: string, title: string): {artist: string; title: string} {
-    const dictionaries = {
-        artist: artist.charAt(0).toUpperCase() + artist.slice(1),
-        title: title.charAt(0).toUpperCase() + title.slice(1)
-    };
-    return dictionaries;
+function show_magicians(magicians: string[]): void {
+    for (const magician of magicians) {
+        console.log(magician.charAt(0).toUpperCase() + magician.slice(1));
+    }
 }
-let album = makeAlbum("ali", "light")
-console.log(album)
-album = makeAlbum("bilal", "red wave")
-console.log(album)
-album = makeAlbum("hamza", "amna")
-console.log(album)
+const magician: string[] = ["ali", "hamza", "bilal"];
+show_magicians(magician)
