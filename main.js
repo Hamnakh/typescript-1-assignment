261944f96002976030e30ecea7874ab0ec44f441
Hamna khan <160540710+Hamnakh@users.noreply.github.com>
2024-02-28 23:41:24 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-function createCar(manufacture, model, optional) {
-    return __assign({ manufacture: manufacture, model: model }, optional);
+//let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
+//for(let user of users) {
+//   if (user === "hamna") {
+//     console.log('Hello hamna, would you like to see a status report?')
+// } else {
+//     console.log('Hello ${user}, thank you for loggin in again') 
+// }
+//}
+//Q.31
+var users = ["hamna", "amna", "aijaz", "night", "arshad"];
+//if (users.length === 0) {
+// console.log ("we need to find users some users!")   
+//} else {
+//  for (let user of users){
+//      if ( user === "hamna") {
+//          console.log('Hello hamna, would you like to see a status report?') 
+//         } else {
+//         console.log('Hello ${user}, thank you for loggin in again')
+//          }
+//     }
+//  }
+//users = []
+//if (users.length === 0) {
+//  console.log ("we need to find some users!")
+//}
+//Q.32
+var current_users = ["admin", "Eric", "Ali", "Hamza", "fatima"];
+var new_users = ["admin", "fatima", "Aliza", "Hasseb", "Noor"];
+var current_user_lower = current_users.map(function (user) { return user.toLowerCase(); });
+for (var _i = 0, new_users_1 = new_users; _i < new_users_1.length; _i++) {
+    var new_user = new_users_1[_i];
+    if (current_user_lower.includes(new_user.toLowerCase())) {
+        console.log("Sorry ".concat(new_user, ", that name is taken"));
+    }
+    else {
+        console.log("yes ".concat(new_user, ", is still in avaliable list"));
+    }
 }
-var mycar = createCar("toyota", "corolla", { color: "silver", year: "2024" });
-console.log(mycar);
--- main.ts
+++ main.ts
@@ -1,14 +1,40 @@
-type car = {
-     manufacture: string
-    model: string
-    [key: string]: any;
- }
- function createCar(manufacture: string, model: string, optional: Record<string, any>): car {
-  return{
-     manufacture,
-    model,
- ...optional
-  }
- }
- const mycar: car = createCar("toyota", "corolla", { color: "silver" , year: "2024"}) 
- console.log(mycar)
+//let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
+//for(let user of users) {
+ //   if (user === "hamna") {
+   //     console.log('Hello hamna, would you like to see a status report?')
+   // } else {
+   //     console.log('Hello ${user}, thank you for loggin in again') 
+   // }
+//}
+
+//Q.31
+let users : string [] = ["hamna", "amna", "aijaz", "night", "arshad"]
+
+//if (users.length === 0) {
+// console.log ("we need to find users some users!")   
+//} else {
+  //  for (let user of users){
+  //      if ( user === "hamna") {
+  //          console.log('Hello hamna, would you like to see a status report?') 
+    
+   //         } else {
+   //         console.log('Hello ${user}, thank you for loggin in again')
+  //          }
+   //     }
+  //  }
+//users = []
+//if (users.length === 0) {
+  //  console.log ("we need to find some users!")
+//}
+
+//Q.32
+let current_users : string [] = ["admin", "Eric", "Ali", "Hamza", "fatima"]
+let new_users : string [] = ["admin", "fatima", "Aliza", "Hasseb", "Noor"]
+let current_user_lower : string [] =current_users.map(user => user.toLowerCase())
+for (let new_user of new_users) {
+    if (current_user_lower.includes (new_user.toLowerCase())) {
+        console.log(`Sorry ${new_user}, that name is taken`)
+    } else {
+        console.log(`yes ${new_user}, is still in avaliable list`)
+    }
+}
